e644aff82d733695d57ac634ca7dd3c49a83dc90
nelonm <acsigy@gmail.com>
2023-10-25 17:49:19 +0200

    for repeat

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+let variable = 10
+variable = 20
+
+const variable2 = 50
+//variable2 = 100
+
+let array = [1, 2, 3]
+array [4, 5, 6]
+
+array.push(10)
+
+console.log (array)
+
+const array2 = [40, 50, 60]
+array2.push(100)
+array2[0]= 11
+
+console.log(array2)
+
+
+const arr = [ "kiss béla", "nagy józsi", "tóth jános", "john doe"]
+
+for ( let i =0; i < arr.length; i++) {
+    console.log (arr[i])
+}
+let lettersLength = 0
+let forEachResult = arr.forEach((value, index) => {
+    //console.log("at index: ", index, " is ", value)
+    console.log(`at index: ${index} is ${value}`)
+    lettersLength += value.length // önálló visszatérési érték nincs, külső elemet módosíthat
+})
+
+let mapResult = arr.map((value, index) => {
+    console.log(`at index: ${index} is ${value}`)
+    return value.length // van saját visszatérési értéke
+})
+
+console.log( "lettersLength: ", lettersLength)
+console.log( "mapResult: ", mapResult)
+
+for (const value of arr) {
+    console.log ( `forof: ${value}`)
+}
+
+for ( let i =0; i < arr[0].length; i++) {
+    console.log (arr[0][i])
+} */
+
